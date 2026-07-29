be8233e03c0f994a9bfc82994d696039a8744518
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:29:34 +0530

    Add Q8_CaseInsensitive.java for case insensitive check

---
+++ Coding challenge programs/Q8_CaseInsensitive.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q8_CaseInsensitive {
+
+    public static void main(String[] args) {
+
+        String str = "java";
+
+        if (str.equalsIgnoreCase("JAVA")) {
+            System.out.println("Match");
+        }
+
+    }
+}
