35daf65764134ac5ed1bbe40bf3e74ebee119f59
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:25:32 +0530

    Add Q1_ArrayIndex.java to print array elements

---
+++ Coding challenge programs/Q1_ArrayIndex.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q1_ArrayIndex {
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
