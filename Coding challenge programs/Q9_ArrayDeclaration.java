d2996cf1716e74014b73594800d4a10370e6cb93
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:30:05 +0530

    Add Q9_ArrayDeclaration.java with array example

---
+++ Coding challenge programs/Q9_ArrayDeclaration.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q9_ArrayDeclaration {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 3};
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
