0b7671aaee2a55ce429ade48dba0f70bc2bff272
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:26:47 +0530

    Add Q3_StringCompare.java for string comparison

---
+++ Coding challenge programs/Q3_StringCompare.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q3_StringCompare {
+
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String s2 = new String("Java");
+
+        if (s1.equals(s2)) {
+            System.out.println("Equal");
+        }
+
+    }
+}
