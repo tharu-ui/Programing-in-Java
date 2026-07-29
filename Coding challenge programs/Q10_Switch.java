2707843381a90f0acc7169305ab522262b3eaded
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:30:33 +0530

    Create Q10_Switch.java

---
+++ Coding challenge programs/Q10_Switch.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q10_Switch {
+
+    public static void main(String[] args) {
+
+        int num = 2;
+
+        switch (num) {
+
+            case 1:
+                System.out.println("One");
+                break;
+
+            case 2:
+                System.out.println("Two");
+                break;
+
+            case 3:
+                System.out.println("Three");
+                break;
+
+            default:
+                System.out.println("Invalid");
+
+        }
+
+    }
+}
