257640a984b0c732ed33352592960e01eefbb468
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:27:23 +0530

    Add Q4_Loop.java with a simple loop example

---
+++ Coding challenge programs/Q4_Loop.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q4_Loop {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
