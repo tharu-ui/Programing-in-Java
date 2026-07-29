426196e3945e3a383bd8d3920e5e7fb7d1477383
Tharuniya <dstharu07@gmail.com>
2026-07-29 20:29:08 +0530

    Add Q7_StringIndex.java to print character at index

---
+++ Coding challenge programs/Q7_StringIndex.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Q7_StringIndex {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+        System.out.println(str.charAt(3));
+
+    }
+}
